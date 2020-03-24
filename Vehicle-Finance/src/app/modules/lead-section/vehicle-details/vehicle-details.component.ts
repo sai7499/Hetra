5e042904197205f5c4df1d4c1a7c2e950fa03497
Sai <sai>
2020-03-24 19:28:11 +0530

    changes

--- Vehicle-Finance/src/app/modules/lead-section/vehicle-details/vehicle-details.component.ts
+++ Vehicle-Finance/src/app/modules/lead-section/vehicle-details/vehicle-details.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
 import { VehicleDetailService} from  '../services/vehicle-detail.service';
+import { LovDataService } from '@services/lov-data.service';
 
 @Component({
   selector: 'app-vehicle-details',
@@ -8,11 +11,12 @@ import { VehicleDetailService} from  '../services/vehicle-detail.service';
 })
 export class VehicleDetailComponent implements OnInit {
 
+    vehicleForm: FormGroup;
 
+    vehicleLov: any  = {};
     public labels = [];
     public errorMsg;
     public getAllFieldLabel;
-    
     public vehicleType:any = [ 'Open', 'Tipper', 'Trailer', 'Goods', 'Bus', 'Passenger Vehicle', 'ODC' ];
     public vechicalRegion: any = ['TN', 'AP'];
     public assetMake: any = [ 'Tata Motors', 'Eicher'];
@@ -28,17 +32,61 @@ export class VehicleDetailComponent implements OnInit {
     public emiProtect:any = ['Yes', 'No'];
     public fastTag:any = ['Yes', 'No'];
     public permitType: any = ['National', 'State', 'Others'];
-  
-    constructor( private vehicleDetailService: VehicleDetailService ) { }
-  
+    constructor( 
+      private vehicleDetailService: VehicleDetailService,
+      private lovDataService: LovDataService
+       ) { }
     ngOnInit() {
+      this.initForm();
       this.getAllFieldLabel = this.vehicleDetailService.getVehicleDetailLabels()
           .subscribe( data => {
-            this.labels = data
-          }, 
+            this.labels = data;
+          },
           error => {
             this.errorMsg = error;
-          });      
+          });
+      this.lovDataService.getLovData().subscribe((value) => {
+        console.log('lov value', value);
+        this.vehicleLov = value;
+      });
     }
-  
+
+    initForm() {
+      this.vehicleForm = new FormGroup({
+      vechicalType: new FormControl(''),
+      region: new FormControl(''),
+      registrationNumber: new FormControl(''),
+      assetMake: new FormControl(''),
+      assetModel: new FormControl(''),
+      assetBodyType: new FormControl(''),
+      assetVariant: new FormControl(''),
+      assetSubVariant: new FormControl(''),
+      monthManufacturing: new FormControl(''),
+      yrManufacturing: new FormControl(''),
+      ageOfAsset: new FormControl(''),
+      vechicalUsage: new FormControl(''),
+      vehicleCategory: new FormControl(''),
+      orpFunding: new FormControl(''),
+      oneTimeTax: new FormControl(''),
+      pac: new FormControl(''),
+      vas: new FormControl(''),
+      emiProduct: new FormControl(''),
+      fastTag: new FormControl(''),
+      others: new FormControl(''),
+      discount: new FormControl(''),
+      finalAssetCost: new FormControl(''),
+      idv: new FormControl(''),
+      insuranceValidity: new FormControl(''),
+      // insurance_copy: new FormControl(''),
+      permitType: new FormControl(''),
+      expiryDate: new FormControl(''),
+      // permit_copy: new FormControl(''),
+      permitOthers: new FormControl(''),
+      frsdRequired: new FormControl(''),
+      frsdAmount: new FormControl(''),
+      fitnessDate: new FormControl(''),
+      // fitness_copy: new FormControl(''),
+      noOfVehicle: new FormControl(''),
+      });
+    }
   }
