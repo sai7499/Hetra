627aacfab0381f3d4043a215349913abcdd23ba1
sai7499 <saikrishna.y85@gmail.com>
2020-04-06 10:48:14 +0530

    credit vehicle-table

--- Vehicle-Finance/src/app/modules/dde/vehicle-details/vehicle-details.component.ts
+++ Vehicle-Finance/src/app/modules/dde/vehicle-details/vehicle-details.component.ts
@@ -9,27 +9,11 @@ import { LabelsService } from '@services/labels.service';
 })
 export class VehicleDetailsComponent implements OnInit {
 
-  basicVehicleDetailForm: FormGroup;
   public label: any = {};
 
   constructor(private _fb: FormBuilder, private labelsData: LabelsService, ) { }
 
   ngOnInit() {
-    this.basicVehicleDetailForm = this._fb.group({
-      vehicleType: ['Open'],
-      region: ['TN'],
-      registrationNumber: [''],
-      assetMake: ['Tata Motors'],
-      assetModel: ['1'],
-      assetBodyType: ['1']
-    })
-    this.labelsData.getLabelsData()
-      .subscribe(data => {
-        this.label = data;
-      },
-        error => {
-          console.log(error, 'error')
-        });
   }
 
 }
