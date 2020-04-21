78736ba3106c5694a387302ec93eeede0361a872
sai7499 <saikrishna.y85@gmail.com>
2020-04-21 11:15:38 +0530

    Login reupdated

--- Vehicle-Finance/src/environments/environment.ts
+++ Vehicle-Finance/src/environments/environment.ts
@@ -1,16 +1,14 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+
+  projectId: '74c36bec6da211eabdc2f2fa9bec3d63',
+
+  api: {
+    'getUserDetails': {
+      'workflowId': '7fde429c82ea11eabdc2f2fa9bec3d63',
+      'processId': '8000bb2e82ea11eabdc2f2fa9bec3d63'
+    }
+  },
+  host: 'http://128.199.164.250/appiyo/'
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
