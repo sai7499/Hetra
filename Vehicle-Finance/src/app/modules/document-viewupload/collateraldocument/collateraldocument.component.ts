99ed7be23faab483e1aa61e03a39a00d0ca99b77
ikalpesh <kalpeshmahajan90@gmail.com>
2020-04-25 16:00:53 +0530

    Collateral DocumentComponent Updated

--- Vehicle-Finance/src/app/modules/document-viewupload/collateraldocument/collateraldocument.component.ts
+++ Vehicle-Finance/src/app/modules/document-viewupload/collateraldocument/collateraldocument.component.ts
@@ -15,15 +15,14 @@ export class CollateraldocumentComponent implements OnInit {
   public labels: any = {};
   collateralDocumentForm: FormGroup;
   vehicleName: any;
+  
 
   constructor(
     private lovData: LovDataService,
     private router: Router,
     private labelsData: LabelsService,
     private fb: FormBuilder) { 
-      this.collateralDocumentForm  = fb.group({
-        documents : fb.array([])
-      })
+     
     }
 
   ngOnInit() {
@@ -32,6 +31,8 @@ export class CollateraldocumentComponent implements OnInit {
       this.values = res[0].collateralDocument[0];
       //console.log(this.values)
     });
+
+    
   }
   initForm() {
  
@@ -45,6 +46,12 @@ export class CollateraldocumentComponent implements OnInit {
       uploadDocument : new FormControl (''),
       status : new FormControl ('')
     })
+
+    this.collateralDocumentForm  = this.fb.group({
+      documents : this.fb.array([
+          this.getUnit()
+      ])
+    });
 
     this.labelsData.getLabelsData().subscribe(
       data => {
@@ -62,24 +69,39 @@ export class CollateraldocumentComponent implements OnInit {
       console.log(this.vehicleName);
   }
 
+  
+
+  addUnit(){
+              const control = <FormArray>this.collateralDocumentForm.controls['documents'];
+              control.push(this.getUnit());
+       }
+
+  removeUnit(i: number) {
+                            
+                          const control = <FormArray>this.collateralDocumentForm.controls['documents'];
+                          control.removeAt(i);
+                        }     
+
+  getUnit() {
+    return this.fb.group({
+      documentName: [('')],
+      documentNumber: [],
+      issueDate: [],
+      documentExpiryDate: [],
+      mandatoryStage: [({value: 'Sales', disabled: true})],
+      deferralDate: [({value:'',disabled:true})],
+      uploadDocument:[],
+      status:[('')]
+
+    });
+  }
+
   onFormSubmit(){
     this.router.navigate(['/pages/lead-section']);
     console.log(this.vehicleName, this.collateralDocumentForm.value);
   }
 
-  addNewAddressGroup(){
-    const add =this.collateralDocumentForm.get('documents') as FormArray;
-    add.push(this.fb.group({
-      documentName: [],
-      documentNumber: [],
-      issueDate: [],
-      documentExpiryDate: [],
-      mandatoryStage: [],
-      deferralDate: [],
-      uploadDocument:[],
-      status:[]
-    }))
-  }
+
   }
 
 
