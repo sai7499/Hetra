f96aabd49eda8d1d1c20db0f9111610c6aaa3cf4
sai7499 <saikrishna.y85@gmail.com>
2020-04-27 12:06:48 +0530

    Revert "New Comp Added"

--- Vehicle-Finance/src/app/modules/dde/dde.routing.module.ts
+++ Vehicle-Finance/src/app/modules/dde/dde.routing.module.ts
@@ -4,7 +4,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { DdeComponent  } from './dde.component';
 import { ApplicantDetailsComponent } from './applicant-details/applicant-details.component';
 import { FlAndPDComponent } from './fl-and-pd/fl-and-pd.component';
-import { VehicleValuationComponent } from './vehicle-valuation/vehicle-valuation.component';
 
 const routes: Routes = [{
     path: '',
@@ -17,10 +16,6 @@ const routes: Routes = [{
         {
             path: 'fl-and-pd',
             component: FlAndPDComponent
-        },
-        {
-            path: 'vehicle-valuation',
-            component: VehicleValuationComponent
         }
     ]
 }];
