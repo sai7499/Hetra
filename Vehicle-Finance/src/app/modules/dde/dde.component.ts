158b2ca3e445ef5234e1132d086609e8135bfbe2
sai7499 <saikrishna.y85@gmail.com>
2020-04-29 15:21:31 +0530

    removedDdeModule

--- Vehicle-Finance/src/app/modules/dde/dde.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    templateUrl: './dde.component.html',
-    styleUrls: [ './dde.component.css']
-})
-export class DdeComponent {
-    constructor() {}
-}
