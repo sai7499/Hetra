852be522f5700dea4088cde198ccaff137f0b579
krishna <harikrishnavkm@gmail.com>
2020-05-06 09:57:26 +0530

    added hover to search bar in dashboard

--- Vehicle-Finance/src/app/app.module.ts
+++ Vehicle-Finance/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './modules/header/header.component';
 // import { LeadSectionModule } from './modules/lead-section/lead-section.module';
-import { LocationStrategy,HashLocationStrategy } from '@angular/common';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './services/auth.interceptor.service';
 
@@ -14,8 +14,7 @@ import { AuthInterceptor } from './services/auth.interceptor.service';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-  
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
--- Vehicle-Finance/src/app/modules/header/header.component.ts
+++ Vehicle-Finance/src/app/modules/header/header.component.ts
@@ -7,26 +7,44 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 })
 export class HeaderComponent implements OnInit, OnDestroy {
   openProfile: boolean;
+  showSearchIcon = true;
 
   bodyClickEvent = event => {
-    if (event.target.id === "profileDropDown") {
+    if (event.target.id === 'profileDropDown') {
       this.openProfile = true;
       return;
     }
     this.openProfile = false;
   };
 
-  constructor() {}
+  constructor() { }
 
   ngOnInit() {
     document
-      .querySelector("body")
-      .addEventListener("click", this.bodyClickEvent);
+      .querySelector('body')
+      .addEventListener('click', this.bodyClickEvent);
   }
 
   ngOnDestroy() {
     document
-      .querySelector("body")
-      .removeEventListener("click", this.bodyClickEvent);
+      .querySelector('body')
+      .removeEventListener('click', this.bodyClickEvent);
   }
+
+  showSearch(event) {
+    if (event.type === 'mouseover') {
+      document.getElementById('myUL').style.display = 'unset';
+
+      this.showSearchIcon = false;
+    }
+  }
+
+  hideSearch(event) {
+    if (event.type === 'mouseout') {
+      document.getElementById('myUL').style.display = 'none';
+      this.showSearchIcon = true;
+
+    }
+  }
+
 }
