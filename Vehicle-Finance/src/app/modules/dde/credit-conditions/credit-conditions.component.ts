aaa2d1ed41e4ab4abc3b3c4e2fe458e75807f5fe
vallabha <vallabha.kudumula549@gmail.com>
2020-05-11 13:54:57 +0530

    adding credit conditions labels in labels json

--- Vehicle-Finance/src/app/modules/dde/credit-conditions/credit-conditions.component.ts
+++ Vehicle-Finance/src/app/modules/dde/credit-conditions/credit-conditions.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LabelsService } from '../../../services/labels.service';
 
 @Component({
   selector: 'app-credit-conditions',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./credit-conditions.component.css']
 })
 export class CreditConditionsComponent implements OnInit {
+  labels: any = {};
+  constructor(
+    public labelsService: LabelsService
+  ) { }
 
-  constructor() { }
+  getLabelData() {
+    this.labelsService.getLabelsData().subscribe(labelsData => {
+      this.labels = labelsData;
+      console.log(this.labels.creditCondition);
+    })
+  }
 
   ngOnInit() {
+    this.getLabelData();
   }
 
 }
