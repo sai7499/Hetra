70b6adca21cd669b7205c70fc993cb7518901709
sai7499 <gomathikumar240496@gmail.com>
2020-05-13 13:52:59 +0530

    colleteral remove field

--- Vehicle-Finance/src/app/modules/dde/vehicle-list/vehicle-list.component.ts
+++ Vehicle-Finance/src/app/modules/dde/vehicle-list/vehicle-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { LabelsService } from '@services/labels.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
     templateUrl: './vehicle-list.component.html',
@@ -8,7 +9,33 @@ import { LabelsService } from '@services/labels.service';
 export class VehicleListComponent {
     public labels: any = {};
 
-    constructor(private labelsData: LabelsService) { }
+    public vehicleListArray = [
+        {
+            registrationNum: 'TN-04-SS-0292',
+            make: 'TATA',
+            model: 'SUMO'
+        },
+        {
+            registrationNum: 'KL-05-AG-1191',
+            make: 'TATA',
+            model: 'SUMO'
+        }
+    ];
+
+    public colleteralArray = [
+        {
+            colleteralType: 'Colleteral Type-1',
+            column2: '',
+            column3: ''
+        },
+        {
+            colleteralType: 'Colleteral Type-2',
+            column2: '',
+            column3: ''
+        }
+    ]
+
+    constructor(private labelsData: LabelsService, private _fb: FormBuilder) { }
 
     ngOnInit() {
         this.labelsData.getLabelsOfDDEData().subscribe(
@@ -19,5 +46,15 @@ export class VehicleListComponent {
                 console.log(error);
             }
         );
+    }
+
+    removeOtherIndex(i, vehicleArray: any) {
+        if (vehicleArray.length > 1) {
+            console.log(i, 'i', vehicleArray.indexOf(i))
+            // control.removeAt(i);
+            vehicleArray.splice(i, 1)
+          } else {
+            alert("Atleast One Row Required");
+          }
     }
 }
