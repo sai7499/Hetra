472fe502fe1e2b56b59b6ffa949315b35a3b2b5b
vallabha <vallabha.kudumula549@gmail.com>
2020-05-15 09:40:33 +0530

    changes in credit condition

--- Vehicle-Finance/src/app/modules/dde/credit-conditions/credit-conditions.component.ts
+++ Vehicle-Finance/src/app/modules/dde/credit-conditions/credit-conditions.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 import { LabelsService } from '../../../services/labels.service';
 
 @Component({
