f4c09247107349f50a3b8a371db5c76314a86671
mahesh <mahesh.harikrishna@gmail.com>
2020-05-16 14:39:07 +0530

    updated header same as dev

--- Vehicle-Finance/src/app/modules/header/header.component.ts
+++ Vehicle-Finance/src/app/modules/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 
 @Component({
@@ -6,46 +6,11 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit, OnDestroy {
-  openProfile: boolean;
-  showSearchIcon = true;
+export class HeaderComponent implements OnInit {
 
-  // bodyClickEvent = event => {
-  //   if (event.target.id === 'profileDropDown') {
-  //     this.openProfile = true;
-  //     return;
-  //   }
-  //   this.openProfile = false;
-  // };
 
   constructor() { }
 
-  ngOnInit() {
-    // document
-    //   .querySelector('body')
-    //   .addEventListener('click', this.bodyClickEvent);
-  }
-
-  ngOnDestroy() {
-    // document
-    //   .querySelector('body')
-    //   .removeEventListener('click', this.bodyClickEvent);
-  }
-
-  // showSearch(event) {
-  //   if (event.type === 'mouseover') {
-  //     document.getElementById('myUL').style.display = 'unset';
-
-  //     this.showSearchIcon = false;
-  //   }
-  // }
-
-  // hideSearch(event) {
-  //   if (event.type === 'mouseout') {
-  //     document.getElementById('myUL').style.display = 'none';
-  //     this.showSearchIcon = true;
-
-  //   }
-  // }
+  ngOnInit() { }
 
 }
