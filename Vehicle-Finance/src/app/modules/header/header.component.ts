c805819720a92c7d5c497032cf7c83b029ede18d
mahesh <mahesh.harikrishna@gmail.com>
2020-05-16 14:33:41 +0530

    updated

--- Vehicle-Finance/src/app/modules/header/header.component.ts
+++ Vehicle-Finance/src/app/modules/header/header.component.ts
@@ -10,42 +10,42 @@ export class HeaderComponent implements OnInit, OnDestroy {
   openProfile: boolean;
   showSearchIcon = true;
 
-  bodyClickEvent = event => {
-    if (event.target.id === 'profileDropDown') {
-      this.openProfile = true;
-      return;
-    }
-    this.openProfile = false;
-  };
+  // bodyClickEvent = event => {
+  //   if (event.target.id === 'profileDropDown') {
+  //     this.openProfile = true;
+  //     return;
+  //   }
+  //   this.openProfile = false;
+  // };
 
   constructor() { }
 
   ngOnInit() {
-    document
-      .querySelector('body')
-      .addEventListener('click', this.bodyClickEvent);
+    // document
+    //   .querySelector('body')
+    //   .addEventListener('click', this.bodyClickEvent);
   }
 
   ngOnDestroy() {
-    document
-      .querySelector('body')
-      .removeEventListener('click', this.bodyClickEvent);
+    // document
+    //   .querySelector('body')
+    //   .removeEventListener('click', this.bodyClickEvent);
   }
 
-  showSearch(event) {
-    if (event.type === 'mouseover') {
-      document.getElementById('myUL').style.display = 'unset';
+  // showSearch(event) {
+  //   if (event.type === 'mouseover') {
+  //     document.getElementById('myUL').style.display = 'unset';
 
-      this.showSearchIcon = false;
-    }
-  }
+  //     this.showSearchIcon = false;
+  //   }
+  // }
 
-  hideSearch(event) {
-    if (event.type === 'mouseout') {
-      document.getElementById('myUL').style.display = 'none';
-      this.showSearchIcon = true;
+  // hideSearch(event) {
+  //   if (event.type === 'mouseout') {
+  //     document.getElementById('myUL').style.display = 'none';
+  //     this.showSearchIcon = true;
 
-    }
-  }
+  //   }
+  // }
 
 }
