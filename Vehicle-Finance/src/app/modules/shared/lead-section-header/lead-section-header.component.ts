060ec22a6321f73bb3793426f342ccd104aacb2b
giripsr769 <giripsr769@gmail.com>
2020-06-03 12:27:58 +0530

    Sales Lead Details

--- Vehicle-Finance/src/app/modules/shared/lead-section-header/lead-section-header.component.ts
+++ Vehicle-Finance/src/app/modules/shared/lead-section-header/lead-section-header.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { LabelsService } from "src/app/services/labels.service";
 import { Routes, RouterModule, Router } from '@angular/router';
+import { LoginStoreService } from '@services/login-store.service';
+import { SharedService } from '@shared/shared-service/shared-service';
+import { CreateLeadDataService } from '../../lead-creation/service/createLead-data.service';
 
 @Component({
   selector: 'app-lead-section-header',
@@ -9,19 +12,50 @@ import { Routes, RouterModule, Router } from '@angular/router';
 })
 export class LeadSectionHeaderComponent implements OnInit {
   labels: any = {};
-  constructor(private labelsData: LabelsService,
-    public router :  Router) {  }
+  userName: string;
+  leadId: any;
+  productId: any;
+  productIdFromLead: any;
+
+  constructor(
+    private labelsData: LabelsService,
+    public router: Router,
+    private loginStoreService: LoginStoreService,
+    private sharedService: SharedService,
+    private createLeadDataService: CreateLeadDataService
+  ) { }
 
   ngOnInit() {
+    this.getLabels();
+    this.sharedService.leadData$.subscribe(value => {
+      this.productId = value;
+    });
+
+    this.getUserDetails();
+
+  }
+
+  getLabels() {
     this.labelsData.getLabelsData().subscribe(
-      data => {
-        this.labels = data;
-        // console.log(this.labels)
-      },
-      error => {
-        console.log(error);
-      }
+      data => this.labels = data,
+      error => console.log(error)
     );
+  }
+
+  getUserDetails() {
+    const roleAndUserDetails = this.loginStoreService.getRolesAndUserDetails();
+    this.userName = roleAndUserDetails.userDetails.firstName;
+
+    const leadSectionData = this.createLeadDataService.getLeadSectionData();
+    this.leadId = (leadSectionData as any).leadId;
+
+    this.sharedService.leadData$.subscribe(value => {
+      this.productId = value;
+    });
+
+
+
+
   }
 
 }
