acc0f818215b14abc35f339b82b7fc0e08bb8c93
krishna <harikrishnavkm@gmail.com>
2020-06-03 16:28:20 +0530

    added leadid to url

--- Vehicle-Finance/src/app/modules/dde/dde.routing.module.ts
+++ Vehicle-Finance/src/app/modules/dde/dde.routing.module.ts
@@ -34,7 +34,7 @@ const routes: Routes = [
     component: DdeComponent,
     children: [
       {
-        path: '',
+        path: ':id',
         component: SourcingDdeComponent,
       },
       {
