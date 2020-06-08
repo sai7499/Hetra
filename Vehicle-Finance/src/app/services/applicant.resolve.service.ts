32824689a4204b1739288acce6cdba9754660a1b
Arun <arun3.ceg@gmail.com>
2020-06-08 11:45:49 +0530

    changes

--- Vehicle-Finance/src/app/services/applicant.resolve.service.ts
+++ Vehicle-Finance/src/app/services/applicant.resolve.service.ts
@@ -5,25 +5,56 @@ import {
 } from '@angular/router';
 import { Injectable } from '@angular/core';
 
-import { ApplicantDetails } from '@model/applicant.model';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { Applicant } from '@model/applicant.model';
 import { HttpService } from '@services/http.service';
 import { ApplicantService } from '@services/applicant.service';
-import { Observable } from 'rxjs';
+import { ApplicantDataStoreService } from '@services/applicant-data-store.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ApplicantResolveService implements Resolve<ApplicantDetails> {
+export class ApplicantResolveService implements Resolve<boolean> {
+  previousApplicantId: number;
   constructor(
     private httpService: HttpService,
-    private applicantService: ApplicantService
+    private applicantService: ApplicantService,
+    private applicantDataStoreService: ApplicantDataStoreService
   ) {}
 
   resolve(
     router: ActivatedRouteSnapshot,
     RouterState: RouterStateSnapshot
-  ): Observable<ApplicantDetails> | ApplicantDetails {
-    console.log('router', router.params);
-    return;
+  ): Observable<boolean> | boolean {
+    const leadId = Number(router.params.leadId);
+    const applicantId = Number(router.firstChild.params.applicantId);
+    if (isNaN(leadId) || isNaN(applicantId)) {
+      return;
+    }
+    if (
+      this.previousApplicantId !== undefined ||
+      this.previousApplicantId === applicantId
+    ) {
+      return;
+    }
+    this.previousApplicantId = applicantId;
+    const data = {
+      applicantId,
+    };
+    return this.applicantService.getApplicantDetail(data).pipe(
+      map((value: any) => {
+        if (value.Error === '0') {
+          const processVariables = value.ProcessVariables;
+          const applicant: Applicant = {
+            ...processVariables,
+          };
+          this.applicantDataStoreService.setApplicant(applicant);
+          return true;
+        }
+        return false;
+      })
+    );
   }
 }
