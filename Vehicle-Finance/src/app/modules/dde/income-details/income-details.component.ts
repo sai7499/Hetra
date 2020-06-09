780050351f90358ff84dc52e769a72f287a60362
krishna <harikrishnavkm@gmail.com>
2020-06-09 21:36:58 +0530

    updated with production build

--- Vehicle-Finance/src/app/modules/dde/income-details/income-details.component.ts
+++ Vehicle-Finance/src/app/modules/dde/income-details/income-details.component.ts
@@ -366,13 +366,9 @@ onSubmit() {
     this.incomeDetailsService
       .setAllIncomeDetails(this.incomeDetailsForm.value)
       .subscribe((res: any) => {
-<<<<<<< HEAD
-        alert(res.ProcessVariables.error.message);
-=======
         if(res["Error"] && res["Error"]==0){
         alert("saved Success")
         }
->>>>>>> ddc5c44b07291617f5e86b30ffb4c5683c364cae
 
       });
   }
