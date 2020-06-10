f80725bbb4b4e62b8a6757a6f62ad985812ca5b6
Arun <arun3.ceg@gmail.com>
2020-06-10 23:14:15 +0530

    changes

---
+++ Vehicle-Finance/src/app/modules/shared/sales-exact-match/sales-exact-match.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './sales-exact-match.component.html',
+  styleUrls: ['./sales-exact-match.component.css'],
+})
+export class SalesExactMatchComponent {}
