98f447a63710991764da85eff2853f5a01b978b8
tejaswinivinod <48154704+tejaswinivinod@users.noreply.github.com>
2020-06-30 21:02:47 +0530

    cam details populated

--- Vehicle-Finance/src/app/modules/dde/cam/cam.component.ts
+++ Vehicle-Finance/src/app/modules/dde/cam/cam.component.ts
@@ -69,7 +69,10 @@ export class CamComponent implements OnInit {
       this.cmRecommendation = res.ProcessVariables['cmRecommendationObj']
       this.acmRecommendation = res.ProcessVariables['acmRecommendationObj']
       this.ncmBhApprovalRecommendation = res.ProcessVariables['ncmBhApprovalRecommendationObj']
-
+this.proposedVehicleDetails.forEach(element => {
+  console.log(element);
+  
+});
 
 
 
