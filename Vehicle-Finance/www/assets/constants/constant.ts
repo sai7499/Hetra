3907c2df210f7eda700c1d2b945b592251e7dd98
sai7499 <saikrishna.y85@gmail.com>
2020-06-30 12:13:07 +0530

    removed www and src folders

--- Vehicle-Finance/www/assets/constants/constant.ts
+++
@@ -1,9 +0,0 @@
-export abstract class Constant {
-  public static readonly ENTITY_INDIVIDUAL_TYPE = 'INDIVENTTYP';
-  public static readonly ENTITY_NON_INDIVIDUAL_TYPE = 'NONINDIVENTTYP';
-  public static readonly CURRENT_ADDRESS = 'CURRADDADDTYP';
-  public static readonly PERMANENT_ADDRESS = 'PERMADDADDTYP';
-  public static readonly COMMUNICATION_ADDRESS = 'COMMADDADDTYP';
-  public static readonly OFFICE_ADDRESS = 'OFFADDADDTYP';
-  public static readonly REGISTER_ADDRESS = 'REGADDADDTYP';
-}
