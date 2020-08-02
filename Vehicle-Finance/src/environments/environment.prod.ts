3f351dda5dd55556e7704f115988c1009bc48415
scmkrishnaangular <r.86krishna@gmail.com>
2020-08-02 23:07:18 +0530

    build revised 0.8

--- Vehicle-Finance/src/environments/environment.prod.ts
+++ Vehicle-Finance/src/environments/environment.prod.ts
@@ -20,14 +20,14 @@ export const environment = {
     wrapperPanValidation : 'db2732f4ab4811ea82f8f2fa9bec3d63',
     wrapperBiometriceKYC : 'db2732f4ab4811ea82f8f2fa9bec3d63'
   },
-  hostingEnvironment: 'DEV',
-  // hostingEnvironment: 'UAT',
+  // hostingEnvironment: 'DEV',
+  hostingEnvironment: 'UAT',
   // hostingEnvironment: 'Production',
 
   host: '/appiyo/',
   //  host: 'http://128.199.164.250/appiyo/',
   // host: 'http://10.101.10.153/appiyo/',
-
+  baseUrl: window.location.origin,
   // tslint:disable-next-line: max-line-length
   aesPublicKey:
     'MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAJ+GJdSSEeaNFBLqyfM3DIOgQgWCwJ0INfeZZV7ITsLeuA7Yd02rrkYGIix1IWvoebWVmzhncUepYxHwK1ARCdUCAwEAAQ==',
