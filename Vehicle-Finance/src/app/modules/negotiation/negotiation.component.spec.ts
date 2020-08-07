9f02956a07bd4bcf1ae7364824784ce56667002b
scmkrishnaangular <r.86krishna@gmail.com>
2020-08-07 14:50:54 +0530

    equtas code added

---
+++ Vehicle-Finance/src/app/modules/negotiation/negotiation.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NegotiationComponent } from './negotiation.component';
+
+describe('NegotiationComponent', () => {
+  let component: NegotiationComponent;
+  let fixture: ComponentFixture<NegotiationComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NegotiationComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NegotiationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
