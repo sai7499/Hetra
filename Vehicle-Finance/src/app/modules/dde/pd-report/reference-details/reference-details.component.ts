aee92db4674b15e4c8ec6c597938f1c5c0f264cd
sai7499 <saikrishna.y85@gmail.com>
2020-08-08 13:34:41 +0530

    routing issue resolved in pd reference detials

--- Vehicle-Finance/src/app/modules/dde/pd-report/reference-details/reference-details.component.ts
+++ Vehicle-Finance/src/app/modules/dde/pd-report/reference-details/reference-details.component.ts
@@ -107,7 +107,7 @@ export class ReferenceDetailsComponent implements OnInit {
     if (this.version !== 'undefined') {
       this.router.navigate([`/pages/pd-dashboard/${this.leadId}/pd-list/${this.applicantId}/income-details/${this.version}`]);
     } else {
-      this.router.navigate([`/pages/pd-dashboard/${this.leadId}/${this.applicantId}/income-details`]);
+      this.router.navigate([`/pages/pd-dashboard/${this.leadId}/pd-list/${this.applicantId}/income-details`]);
     }
   }
 
