c20905b20d6ee1ae0a1836455a03ba79f5e5a398
scmkrishnaangular <r.86krishna@gmail.com>
2020-08-09 13:46:58 +0530

    sanction leter updated

--- Vehicle-Finance/src/app/services/auth.interceptor.service.ts
+++ Vehicle-Finance/src/app/services/auth.interceptor.service.ts
@@ -107,7 +107,7 @@ export class AuthInterceptor implements HttpInterceptor {
             this.checkApiCount();
             return event;
           } else {
-            this.ngxUiLoaderService.stop();
+            // this.ngxUiLoaderService.stop();
           }
         },
         (err: any) => {
