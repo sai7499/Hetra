7e46fe162d1f28888050ee920ffc5f75723d939b
Krish yadagani <53928270+sai7499@users.noreply.github.com>
2020-08-09 13:39:22 +0530

    changed host url to appiyo in environment.ts (#580)

--- Vehicle-Finance/src/app/modules/dde/fi-list/fi-list.component.ts
+++ Vehicle-Finance/src/app/modules/dde/fi-list/fi-list.component.ts
@@ -21,6 +21,8 @@ export class FiListComponent implements OnInit {
   userId: any;
   show: boolean;
   showStatus: boolean;
+  fiStatusValue: any;
+  fiStatus: { [id: string]: any; } = {};
   constructor(
     private labelDetails: LabelsService,
     private router: Router,
@@ -56,7 +58,7 @@ export class FiListComponent implements OnInit {
     this.getFiList();
     if (this.router.url.includes('/fi-dashboard')) {   // showing/hiding the buttons based on url
 
-      console.log(' pd-dashboard ');
+      console.log(' fi-dashboard ');
       this.show = true;
 
     } else if (this.router.url.includes('/dde')) {
@@ -108,16 +110,61 @@ export class FiListComponent implements OnInit {
     this.fieldInvestigationService.getFiList(data).subscribe((value: any) => {
       const processvariables = value.ProcessVariables;
       // console.log('in get fi list', processvariables);
-      this.fiList = processvariables.fIReportList;
+      this.fiList = processvariables.finalFIList;
       console.log('fi List', this.fiList);
+
+      for (var i in this.fiList) {
+        this.fiStatusValue = this.fiList[i]['fiStatusValue']
+        if (this.fiList[i]['fiStatusValue'] == "Submitted") {
+          this.fiStatus[this.fiList[i]['applicantId']] = this.fiList[i]['fiStatusValue']
+          // this.sharedService.getPdStatus(this.pdStatus)
+        }
+
+      }
     });
   }
 
 
   onClick(applicantIdFromHtml: string) {
+
     console.log('this applicant id', applicantIdFromHtml);
-    this.router.navigateByUrl(`pages/fi-list/${this.leadId}/${applicantIdFromHtml}/fi-report/fi-residence`);
+    this.router.navigateByUrl(`pages/fi-dashboard/${this.leadId}/fi-report/${applicantIdFromHtml}/fi-residence`);
   }
+  navigatePage(applicantId: string, version: any) {
+    console.log(
+      'applicantId',
+      applicantId,
+    );
+    console.log('version', version);
+
+    console.log('URL', URL);
+
+    if (this.router.url.includes('fi-dashboard')) {
+
+      console.log(' in fi-dashboard flow');
+
+      // this.show = true;
+      if (version) {
+        console.log('in fi-dashboard version conditon');
+        this.router.navigate([`/pages/fi-dashboard/${this.leadId}/fi-report/${applicantId}/fi-residence/${version}`]);
+      } else if (version === undefined || version === null) {
+        console.log('in fi-cum-pd-dashboard undefined version conditon');
+        this.router.navigate([`/pages/fi-dashboard/${this.leadId}/fi-report/${applicantId}/fi-residence`]);
+      }
+
+    } else if (this.router.url.includes('/dde')) {
+      console.log(' in dde flow');
+      // this.showStatus = true;
+      if (version) {
+        this.router.navigate([`/pages/dde/${this.leadId}/fi-report/${applicantId}/fi-residence/${version}`]);
+      } else if (version === undefined || version === null) {
+
+        this.router.navigate([`/pages/dde/${this.leadId}/fi-report/${applicantId}/fi-residence`]);
+      }
+
+    }
+  }
+
 
   onNavigate(action) { // fun for routing into next and back pages using argument ==> 'action'
     // console.log('in on navigate', action);
