ea809d065cc9a2e08593dcaea98a39a70ac11cc9
tejaswinivinod <48154704+tejaswinivinod@users.noreply.github.com>
2020-08-17 18:23:55 +0530

    pdd api

--- Vehicle-Finance/src/app/modules/dde/pdd-details/pdd-details.component.ts
+++ Vehicle-Finance/src/app/modules/dde/pdd-details/pdd-details.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { LabelsService } from '@services/labels.service';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormControl } from '@angular/forms';
 import { CommomLovService } from '@services/commom-lov-service';
 import { ToasterService } from '@services/toaster.service';
+import { PddDetailsService } from '@services/pdd-details.service';
 
 @Component({
   selector: 'app-pdd-details',
@@ -12,34 +13,56 @@ import { ToasterService } from '@services/toaster.service';
 })
 export class PddDetailsComponent implements OnInit {
   labels: any = {};
-  pddDetailsForm : any;
+  pddDetailsForm: any;
   isDirty = false;
-
+  leadId: number;
+  userId: string;
+  applicantResponse: any;
+  childgroups = [];
   constructor(
     private router: Router,
     private labelsData: LabelsService,
     private formBuilder: FormBuilder,
     private commonLovService: CommomLovService,
     private activatedRoute: ActivatedRoute,
-    private toasterService: ToasterService
+    private toasterService: ToasterService,
+    private pddDetailsService: PddDetailsService
   ) { }
 
   ngOnInit() {
-    
+
     this.getLabels();
+    this.getLeadId();
+    this.userId = localStorage.getItem('userId');
     this.getLov();
+    // this.getAllPddDetails();
+    const data = {
+      leadId: this.leadId,
+      userId: this.userId
+    }
     this.pddDetailsForm = this.formBuilder.group({
-      documentName:[""],
-      expectedDate:[""],
-      documentNumber:[""],
-      viewDocument:[""],
-      collectedDate:[""],
-      status:[""],
-      cpcCheck:[""],
-      physicalTrackingNumber:[""],
-     
+      pddDocumentDetails: this.formBuilder.array(this.childgroups),
     })
+    this.pddDetailsService.getPddDetails(data).subscribe((res: any) => {
+      console.log("getting response", res)
+      this.applicantResponse = res.ProcessVariables.pddDocumentDetails;
+      console.log(this.applicantResponse);
+      // this.getpddDocumentDetails(this.applicantResponse)
+      for(let i = 0; i < this.applicantResponse.length; i++){
+        this.childgroups.push(this.getpddDocumentDetails(this.applicantResponse[i]));
+      }
+      this.pddDetailsForm = this.formBuilder.group({
+        pddDocumentDetails: this.formBuilder.array(this.childgroups),
+      })
+    })
+   
+    console.log(this.childgroups);
+    
+    
+    console.log("form controls", this.pddDetailsForm.value);
   }
+
+
   // getting labels from labels.json
   getLabels() {
     this.labelsData.getLabelsData().subscribe(
@@ -50,13 +73,81 @@ export class PddDetailsComponent implements OnInit {
       (error) => { }
     );
   }
+  //getting leadID from Pdd Dashboard 
+  getLeadId() {
+    return new Promise((resolve, reject) => {
+      this.activatedRoute.parent.params.subscribe((value) => {
+        if (value && value.leadId) {
+          resolve(Number(value.leadId));
+          this.leadId = Number(value.leadId);
+        }
+        resolve(null);
+      });
+    });
+  }
   //getting LOV's from commonLovService
   getLov() {
     this.commonLovService.getLovData().subscribe((value: any) => {
-      console.log("values",value);
-      
+      console.log("values", value);
+
     });
   }
+
+  private getpddDocumentDetails(data?: any) {
+    console.log("res from get", data);
+
+    // if (data ) {
+    //   if (data && data.length > 0) {
+        // tslint:disable-next-line: prefer-for-of
+        // for (let i = 0; i < data.length; i++) {
+
+          return this.formBuilder.group({
+
+            documentName: new FormControl(data.documentName),
+            expectedDate: new FormControl(data.expectedDate),
+            documentNumber: new FormControl(data.documentNumber),
+            viewDocument: new FormControl(data.viewDocument),
+            collectedDate: new FormControl(data.collectedDate),
+            status: new FormControl(data.status),
+            cpcCheck: new FormControl(data.cpcCheck),
+            physicalTrackingNumber: new FormControl(data.physicalTrackingNumber),
+          });
+        // }
+      // }
+     
+    // } else if(data === undefined) {
+    //     return this.formBuilder.group({
+    //       documentName: Number(data.documentName ? data.documentName : ''),
+    //       documentNumber: data.documentNumber ? data.documentNumber : '',
+    //       viewDocument: data.viewDocument
+    //         ? data.viewDocument
+    //         : '',
+    //       collectedDate: data.collectedDate ? data.collectedDate : '',
+    //       status: Number(data.status ? data.status : ''),
+    //       cpcCheck: Number(data.cpcCheck ? data.cpcCheck : ''),
+    //       physicalTrackingNumber: Number(data.physicalTrackingNumber ? data.physicalTrackingNumber : ''),
+    //     });
+    // }
+  }
+  //getting pdd details from getPddDetails api 
+  // getAllPddDetails() {
+  //   const data = {
+  //     leadId: this.leadId,
+  //     userId: this.userId
+  //   }
+  //   this.pddDetailsService.getPddDetails(data).subscribe((res: any) => {
+  //     console.log("getting response", res)
+  //     this.applicantResponse = res.ProcessVariables.pddDocumentDetails;
+  //     console.log(this.applicantResponse);
+  //     // this.getpddDocumentDetails(this.applicantResponse)
+  //     for(let i = 0; i < this.applicantResponse.length; i++){
+  //       this.childgroups.push(this.getpddDocumentDetails(this.applicantResponse[i]));
+  //     }
+     
+  //   })
+
+  // }
+
   onSubmit() {
     this.router.navigate([`/pages/dashboard`]);
   }
--- Vehicle-Finance/src/app/services/pdd-details.service.ts
+++ Vehicle-Finance/src/app/services/pdd-details.service.ts
@@ -1,9 +1,62 @@
 import { Injectable } from '@angular/core';
+import { HttpService } from '../services/http.service';
+
+import { environment } from 'src/environments/environment';
+import RequestEntity from '@model/request.entity';
+import { ApiService } from './api.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PddDetailsService {
 
-  constructor() { }
+  constructor( private httpService: HttpService,  private apiService: ApiService) { }
+
+  getPddDetails(data) {
+
+    const processData = data;
+    const processId = this.apiService.api.getPddDetails.processId;
+    const workflowId = this.apiService.api.getPddDetails.workflowId;
+    const projectId = this.apiService.api.getPddDetails.projectId;
+
+    const userId = localStorage.getItem('userId');
+
+    const requestEntity: RequestEntity = {
+        processId,
+        ProcessVariables:  processData,
+        workflowId,
+        projectId
+    };
+    console.log(requestEntity);
+    
+    let url = environment.host + 'd/workflows/' + workflowId + '/' + environment.apiVersion.api + 'execute?projectId=' + projectId;
+    console.log(url);
+    
+    return this.httpService.post(url, requestEntity);
+    
+}
+updatePddDetails(data) {
+
+  const processData = data;
+  const processId = this.apiService.api.getPddDetails.processId;
+  const workflowId = this.apiService.api.getPddDetails.workflowId;
+  const projectId = this.apiService.api.getPddDetails.projectId;
+
+  const userId = localStorage.getItem('userId');
+
+  const requestEntity: RequestEntity = {
+      processId,
+      ProcessVariables:  processData,
+      workflowId,
+      projectId
+  };
+  console.log(requestEntity);
+  
+  let url = environment.host + 'd/workflows/' + workflowId + '/' + environment.apiVersion.api + 'execute?projectId=' + projectId;
+  console.log(url);
+  
+  return this.httpService.post(url, requestEntity);
+  
+}
 }
