392ceedc4818fcaef8303815568d3421bd61b243
tejaswinivinod <48154704+tejaswinivinod@users.noreply.github.com>
2020-08-19 14:17:46 +0530

    pdd integration,cam issues

--- Vehicle-Finance/src/app/modules/dde/pdd-details/pdd-details.component.ts
+++ Vehicle-Finance/src/app/modules/dde/pdd-details/pdd-details.component.ts
@@ -5,6 +5,9 @@ import { FormBuilder, FormControl, FormArray } from '@angular/forms';
 import { CommomLovService } from '@services/commom-lov-service';
 import { ToasterService } from '@services/toaster.service';
 import { PddDetailsService } from '@services/pdd-details.service';
+import { UploadService } from '@services/upload.service';
+import { DraggableContainerService } from '@services/draggable.service';
+import { UtilityService } from '@services/utility.service';
 
 @Component({
   selector: 'app-pdd-details',
@@ -21,6 +24,8 @@ export class PddDetailsComponent implements OnInit {
   childgroups = [];
   submitted: boolean;
   pddLov: any = [];
+  public toDayDate: Date = new Date();
+
   constructor(
     private router: Router,
     private labelsData: LabelsService,
@@ -28,7 +33,12 @@ export class PddDetailsComponent implements OnInit {
     private commonLovService: CommomLovService,
     private activatedRoute: ActivatedRoute,
     private toasterService: ToasterService,
-    private pddDetailsService: PddDetailsService
+    private pddDetailsService: PddDetailsService,
+    private uploadService: UploadService,
+    private draggableContainerService: DraggableContainerService,
+    private utilityService: UtilityService,
+
+
   ) { }
 
   ngOnInit() {
@@ -117,14 +127,14 @@ export class PddDetailsComponent implements OnInit {
      {
       return this.formBuilder.group({
         documentId: data.documentId ? data.documentId : null,
-        documentName: data.documentTypeValue ? data.documentTypeValue : '',
-        expectedDate: (data.deferredDate ? data.deferredDate : ''),
-        documentNumber: data.documentNumber ? data.documentNumber : '',
-        dmsDocumentId: data.dmsDocumentId ? data.dmsDocumentId : '',
-        docCollectedDate: data.docCollectedDate ? data.docCollectedDate : '',
-        pddDocumentStatus: data.pddDocumentStatus ? data.pddDocumentStatus : '',
-        cpcCheck: data.cpcCheck ? data.cpcCheck : '',
-        physicalTrackingNumber:data.physicalTrackingNumber ? data.physicalTrackingNumber : '',
+        documentName: data.documentTypeValue ? data.documentTypeValue : null,
+        expectedDate: (data.deferredDate ? data.deferredDate : null),
+        documentNumber: data.documentNumber ? data.documentNumber : null,
+        dmsDocumentId: data.dmsDocumentId ? data.dmsDocumentId : null,
+        docCollectedDate: data.docCollectedDate ? this.utilityService.getDateFromString(data.docCollectedDate) : null,
+        pddDocumentStatus: data.pddDocumentStatus ? data.pddDocumentStatus : null,
+        cpcCheck: data.cpcCheck ? data.cpcCheck : null,
+        physicalTrackingNumber:data.physicalTrackingNumber ? data.physicalTrackingNumber : null,
 
       });
     }
@@ -212,6 +222,7 @@ export class PddDetailsComponent implements OnInit {
     })
 
   }
+
   onSubmit() {
     console.log(this.pddDetailsForm);
 
@@ -225,7 +236,29 @@ export class PddDetailsComponent implements OnInit {
       return;
     } else {
       this.submitted = true;
-      this.pddDetailsService.updatePddDetails(this.pddDetailsForm.value).subscribe((res: any) => {
+      this.pddDetailsForm.value.pddDocumentDetails.forEach(ele => {
+        // ele.cpcCheck =  ele.cpcCheck.toString();
+        // ele.dmsDocumentId = ele.dmsDocumentId.toString();
+        if(ele.docCollectedDate != null){
+        ele.docCollectedDate =this.utilityService.convertDateTimeTOUTC(
+          ele.docCollectedDate,
+          'DD/MM/YYYY'
+        );
+        }
+        // ele.documentId = Number(ele.documentId);
+        // ele.documentNumber = ele.documentNumber.toString();
+        // ele.pddDocumentStatus = ele.pddDocumentStatus.toString();
+        // ele.physicalTrackingNumber = ele.physicalTrackingNumber.toString();
+      });
+      const body = {
+        pddDocumentDetails: this.pddDetailsForm.value.pddDocumentDetails
+
+      }
+      console.log(body);
+      
+      this.pddDetailsService.updatePddDetails(body).subscribe((res: any) => {
+        console.log(res);
+        
         // tslint:disable-next-line: triple-equals
         if (res && res.ProcessVariables.error.code == '0') {
           // tslint:disable-next-line: prefer-const
@@ -246,6 +279,66 @@ export class PddDetailsComponent implements OnInit {
         }
       });
     }
+  }
+  getBase64String(dmsDocumentId) {
+    console.log(dmsDocumentId);
+    
+    return new Promise((resolve, reject) => {
+      this.uploadService
+        .getDocumentBase64String(dmsDocumentId)
+        .subscribe((value) => {
+
+          const imageUrl = value['dwnldDocumentRep'].msgBdy.bsPyld;
+          const documentName = value['dwnldDocumentRep'].msgBdy.docNm || '';
+          const imageType = documentName.split('.')[1].toLowerCase();
+
+          resolve({
+            imageUrl,
+            imageType,
+          });
+          console.log('downloadDocs', value);
+        });
+    });
+  }
+  //for document upload
+  async downloadDocs(event) {
+    // let el = event.srcElement;
+    console.log(event);
+    const dmsDocumentID : any = await this.getBase64String(event)
+    console.log(dmsDocumentID)
+    // const formArray = this.uploadForm.get(formArrayName) as FormArray;
+    // const documentId = formArray.at(index).get('file').value;
+    // if (!documentId) {
+    //   return;
+    // }
+    // const bas64String = this.base64StorageService.getString(
+    //   this.applicantId + documentId
+    // );
+    // if (bas64String) {
+    //   this.setContainerPosition(el);
+      const showDraggableContainer = {
+        imageUrl: dmsDocumentID.imageUrl,
+        imageType: dmsDocumentID.imageType,
+      };
+      this.draggableContainerService.setContainerValue({
+        image: showDraggableContainer,
+      });
+    //   return;
+    // }
+    // const imageValue: any = await this.getBase64String(documentId);
+    // this.setContainerPosition(el);
+    // this.showDraggableContainer = {
+    //   imageUrl: imageValue.imageUrl,
+    //   imageType: imageValue.imageType,
+    // };
+    // this.draggableContainerService.setContainerValue({
+    //   image: this.showDraggableContainer,
+    //   css: this.setCss,
+    // });
+    // this.base64StorageService.storeString(this.applicantId + documentId, {
+    //   imageUrl: imageValue.imageUrl,
+    //   imageType: imageValue.imageType,
+    // });
   }
   // onSubmit() {
 
