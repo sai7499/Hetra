be89c56defd42d3a2f846383b1ec9d8e3adb7932
GomathiKumar24 <gomathikumar240496@gmail.com>
2020-09-07 18:30:23 +0530

    additional collaterals

--- Vehicle-Finance/src/app/modules/dde/vehicle-details/additional-collateral-details/additional-collateral-details.component.ts
+++ Vehicle-Finance/src/app/modules/dde/vehicle-details/additional-collateral-details/additional-collateral-details.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { LabelsService } from '@services/labels.service';
 import { CreateLeadDataService } from '@modules/lead-creation/service/createLead-data.service';
+import { CommomLovService } from '@services/commom-lov-service';
 
 @Component({
     selector: 'app-additional-collateral-details',
@@ -15,48 +16,131 @@ export class AdditionalCollateralComponent implements OnInit {
     public LOV: any = {};
 
     leadId: number;
+    isDirty: boolean;
+    collateralType: string = '0';
 
-    constructor(private _fb: FormBuilder, private labelsData: LabelsService, private createLeadDataService: CreateLeadDataService) { }
+    constructor(private _fb: FormBuilder, private labelsData: LabelsService, private createLeadDataService: CreateLeadDataService,
+        private commonLovService: CommomLovService) { }
 
     ngOnInit() {
 
         this.collateralForm = this._fb.group({
             collateralType: ['0', Validators.required],
+            propertyOwnership: [''],
+            proofCollected: [''],
+            nameofProof: [''],
             collateralFormArray: this._fb.array([])
         })
+
+        this.labelsData.getLabelsData()
+            .subscribe(data => {
+                this.label = data;
+            }, error => {
+                console.log('error', error)
+            });
 
         let leadData = this.createLeadDataService.getLeadSectionData();
         this.leadId = leadData['leadId'];
 
-        this.labelsData.getLabelsData().subscribe(data => {
-            this.label = data;
-        }, error => {
-            console.log('error', error)
-        });
-
-        this.LOV.collateralType = [
-            {
-                key: '0',
-                value: 'Agriculture'
-            },
-            {
-                key: '1',
-                value: 'FD'
-            }, {
-                key: '2',
-                value: 'Gold_Loan'
-            },
-            {
-                key: '3',
-                value: 'Property'
-            },
-        ]
+        this.initForm();
+        this.getLov();
     }
 
     selectCollateralType(value) {
-        console.log(value, 'value')
-        console.log(this.collateralForm.get('collateralType').value, 'Fotm')
-        // this.collateralForm.get('collateralType').value = value;
+        this.collateralType = value;
+        this.initForm();
     }
 
+    getLov() {
+        this.commonLovService.getLovData().subscribe((value: any) => {
+            this.LOV = value.LOVS;
+            this.LOV.collateralType = [
+                {
+                    key: '0',
+                    value: 'Agriculture'
+                },
+                {
+                    key: '1',
+                    value: 'FD'
+                }, {
+                    key: '2',
+                    value: 'Gold_Loan'
+                },
+                {
+                    key: '3',
+                    value: 'Property'
+                },
+            ]
+            this.LOV.YesORNoValue = [
+                {
+                  key: "1",
+                  value: "Yes"
+                },
+                {
+                  key: "0",
+                  value: "NO"
+                }
+              ]
+        })
+    }
+
+    initForm() {
+
+        // const formArray = (this.collateralForm.get('collateralFormArray') as FormArray);
+        // formArray.clear();
+        this.collateralType === '0' ? this.addAgricultureFormControl() : this.collateralType === '1' ?
+        this.addFDFormControl() : this.collateralType === '2' ? this.addGoldLoanFormControl() : this.addPropertyFormControl();
+
+    }
+
+    addAgricultureFormControl() {
+        const formArray = (this.collateralForm.get('collateralFormArray') as FormArray);
+        formArray.clear();
+        formArray.push(this._fb.group({
+            surveyNumber: ['', Validators.required],
+            landInAcres: ['', Validators.required],
+            propertyOwner: ['', Validators.required],
+            relationWithApplicant: ['', Validators.required],
+            marketValue: ['', Validators.required],
+            totalMarketValue: ['', Validators.required],
+            guideLineValue: ['', Validators.required],
+            totalGuideLineValue: ['', Validators.required],
+            jewelOwner: ['', Validators.required]
+        }))
+
+    }
+
+    addFDFormControl() {
+        const formArray = (this.collateralForm.get('collateralFormArray') as FormArray);
+        formArray.clear();
+        formArray.push(this._fb.group({
+            fdAccountNo: ['', Validators.required],
+            fdName: ['', Validators.required],
+            fdValue: ['', Validators.required],
+        }))
+    }
+
+    addGoldLoanFormControl() {
+        const formArray = (this.collateralForm.get('collateralFormArray') as FormArray);
+        formArray.clear();
+        formArray.push(this._fb.group({
+            goldInGrams: ['', Validators.required],
+            currentValuePerGram: ['', Validators.required],
+            goldTotalValue: ['', Validators.required],
+            purity: ['', Validators.required]
+        }))
+    }
+
+    addPropertyFormControl() {
+        const formArray = (this.collateralForm.get('collateralFormArray') as FormArray);
+        formArray.clear();
+        formArray.push(this._fb.group({
+            totalLandArea: ['', Validators.required],
+            totalBuiltUpArea: ['', Validators.required],
+            marketValue: ['', Validators.required],
+            totalMarketValue: ['', Validators.required],
+            guideLineValue: ['', Validators.required],
+            totalGuideLineValue: ['', Validators.required],
+        }))
+    }
 }
