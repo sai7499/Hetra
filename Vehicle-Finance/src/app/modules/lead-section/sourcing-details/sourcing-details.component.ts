acacbf5733aa8ef570b6f3f0e1a901979782d53b
giripsr769 <giripsr769@gmail.com>
2020-09-09 14:27:33 +0530

    app no.

--- Vehicle-Finance/src/app/modules/lead-section/sourcing-details/sourcing-details.component.ts
+++ Vehicle-Finance/src/app/modules/lead-section/sourcing-details/sourcing-details.component.ts
@@ -569,7 +569,7 @@ export class SourcingDetailsComponent implements OnInit {
       leadHandeledBy: new FormControl('', Validators.required),
       productCategory: new FormControl('', Validators.required),
       priority: new FormControl(''),
-      applicationNo: new FormControl('', Validators.required),
+      applicationNo: new FormControl(''),
       product: new FormControl('', Validators.required),
       fundingProgram: new FormControl('', Validators.required),
       bizDivision: new FormControl('', Validators.required),
