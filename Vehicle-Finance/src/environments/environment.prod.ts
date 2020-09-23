69ecef547108c61825b2da121aa89f440cc1245a
scmkrishnaangular <r.86krishna@gmail.com>
2020-09-23 20:24:12 +0530

    build released 0.17.3

--- Vehicle-Finance/src/environments/environment.prod.ts
+++ Vehicle-Finance/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  version: '0.17.2',
-  buildDate: '22/09/2020',
+  version: '0.17.4',
+  buildDate: '23/09/2020',
   apiVersion: {
     login: 'v3/',
     api: 'v2/',
