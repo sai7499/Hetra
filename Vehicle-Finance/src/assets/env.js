96ab95112b76eeb23cb302e63ab4b9fecfbcc80f
scmkrishnaangular <r.86krishna@gmail.com>
2020-10-16 13:28:30 +0530

    build released 0.23.4

--- Vehicle-Finance/src/assets/env.js
+++ Vehicle-Finance/src/assets/env.js
@@ -8,6 +8,6 @@
     // window["env"]["userConfig"] = "@esfbuat.in";
     window["env"]["userConfig"] = "@equitasbank.in";
     window["env"]["useADAuth"] = false;
-    window["env"]["version"] = "0.23.3"; 
-    window["env"]["buildDate"] = "15-10-2020";
+    window["env"]["version"] = "0.23.4"; 
+    window["env"]["buildDate"] = "16-10-2020";
   })(this);
