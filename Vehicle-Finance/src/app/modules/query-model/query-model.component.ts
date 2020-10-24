d96b8ad079a5bed51e867e6b300bfa46eea3351e
GomathiKumar24 <gomathikumar240496@gmail.com>
2020-10-24 11:47:33 +0530

    Query Model

--- Vehicle-Finance/src/app/modules/query-model/query-model.component.ts
+++ Vehicle-Finance/src/app/modules/query-model/query-model.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-query-model',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QueryModelComponent implements OnInit {
 
-  constructor() { }
+  queryModalForm: FormGroup;
+  chatList: any = []
+
+  constructor(private _fb: FormBuilder) { }
 
   ngOnInit() {
+    this.queryModalForm = this._fb.group({
+      
+    })
   }
 
 }
