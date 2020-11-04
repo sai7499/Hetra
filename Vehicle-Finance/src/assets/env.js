83bb8605103ee2097fb7e2e1ee3770261882941b
christus valerian <christusvalerian@christuss-MacBook-Air.local>
2020-11-04 15:23:07 +0530

    update in env

--- Vehicle-Finance/src/assets/env.js
+++ Vehicle-Finance/src/assets/env.js
@@ -8,13 +8,7 @@
     // window["env"]["userConfig"] = "@esfbuat.in";
     window["env"]["userConfig"] = "@equitasbank.in";
     window["env"]["useADAuth"] = false;
-<<<<<<< HEAD
-    window["env"]["version"] = "0.26.3"; 
-    window["env"]["buildDate"] = "24-10-2020";
-  })(this);
-=======
     window["env"]["version"] = "0.27.1"; 
     window["env"]["buildDate"] = "01-11-2020";
   })(this);
   
->>>>>>> 2a3719b55cf173e199fde87bc31fbd0d6315631b
