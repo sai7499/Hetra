3a9639010a46ed9bebf53159f2d0d5925907005a
krishna <harikrishnavkm@gmail.com>
2020-11-06 15:40:48 +0530

    updated dashboard with new RCU login

--- Vehicle-Finance/src/app/modules/dashboard/dashboard/dashboard.component.ts
+++ Vehicle-Finance/src/app/modules/dashboard/dashboard/dashboard.component.ts
@@ -60,7 +60,10 @@ export enum DisplayTabs {
   PDDWithMe,
   PDDWithBranch,
   ReversedLeadsWithMe,
-  ReversedLeadsWithBranch
+  ReversedLeadsWithBranch,
+  RCU,
+  RCUWithMe,
+  RCUWithBranch
 }
 
 export enum sortingTables {
@@ -203,6 +206,10 @@ export class DashboardComponent implements OnInit {
         this.subActiveTab = 34;
         this.onTabsLoading(this.subActiveTab);
         this.onLeads(this.displayTabs.CPCChecker, this.displayTabs.CPCCheckerWithMe, 'CPC');
+      } else if (this.roleType === 6) {
+        this.activeTab = 44;
+        this.subActiveTab = 45;
+        this.onTabsLoading(this.subActiveTab);
       }
     }
 
@@ -362,7 +369,7 @@ export class DashboardComponent implements OnInit {
   }
 
   onFromDateChange() {
-    if(!this.displayTabs.PDD && !this.displayTabs.PDDforCPC) {
+    if (!this.displayTabs.PDD && !this.displayTabs.PDDforCPC) {
 
     }
     this.filterForm.get('fromDate').valueChanges.pipe(debounceTime(0)).subscribe((data) => {
@@ -410,12 +417,12 @@ export class DashboardComponent implements OnInit {
         break;
     }
     switch (data) {
-      case 4: case 6: case 8: case 10: case 13: case 21: case 23: case 25: case 28: case 31: case 34: case 37: case 40: case 42:
+      case 4: case 6: case 8: case 10: case 13: case 21: case 23: case 25: case 28: case 31: case 34: case 37: case 40: case 42: case 45:
         this.onAssignTab = false;
         this.onReleaseTab = true;
         this.myLeads = true;
         break;
-      case 5: case 7: case 9: case 11: case 14: case 22: case 24: case 26: case 29: case 32: case 35: case 38: case 41: case 43:
+      case 5: case 7: case 9: case 11: case 14: case 22: case 24: case 26: case 29: case 32: case 35: case 38: case 41: case 43: case 46:
         this.onAssignTab = true;
         this.onReleaseTab = false;
         this.myLeads = false;
@@ -482,8 +489,12 @@ export class DashboardComponent implements OnInit {
         this.taskName = 'CPC-PDD';
         this.getTaskDashboardLeads(this.itemsPerPage, event);
         break;
-        case 42: case 43:
+      case 42: case 43:
         this.taskName = 'Send Back To Sales';
+        this.getTaskDashboardLeads(this.itemsPerPage, event);
+        break;
+      case 45: case 46:
+        this.taskName = 'RCU';
         this.getTaskDashboardLeads(this.itemsPerPage, event);
         break;
       default:
@@ -799,8 +810,11 @@ export class DashboardComponent implements OnInit {
       case 40: case 41:
         this.router.navigateByUrl(`/pages/pdd/${this.leadId}`);
         break;
-        case 42: case 43:
-          this.router.navigateByUrl(`/pages/sales/${this.leadId}/lead-details`);
+      case 42: case 43:
+        this.router.navigateByUrl(`/pages/sales/${this.leadId}/lead-details`);
+        break;
+      case 45: case 46:
+        this.router.navigateByUrl(`/pages/sales/${this.leadId}/lead-details`);
         break;
 
       default:
