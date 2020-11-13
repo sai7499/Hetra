f516111e3f43246a56877c06f30c04b7f6dab340
sai7499 <saikrishna.y85@gmail.com>
2020-11-13 17:02:31 +0530

    issue resloved in fi list screen for send back to sales condition

--- Vehicle-Finance/src/app/modules/dde/fi-list/fi-list.component.ts
+++ Vehicle-Finance/src/app/modules/dde/fi-list/fi-list.component.ts
@@ -99,7 +99,9 @@ export class FiListComponent implements OnInit {
       console.log('fi List', this.fiList);
 
       for (var i in this.fiList) {
-        this.fiStatusValue = this.fiList[i]['fiStatus']
+        if (this.fiList[i]['fiStatus']!= null) {
+          this.fiStatusValue = this.fiList[i]['fiStatus']
+        }
 
         if (this.fiList[i]['fiStatusValue'] == "Submitted") {
           this.fiStatus[this.fiList[i]['applicantId']] = this.fiList[i]['fiStatusValue']
