1c29f8d6694ff6df1344df98c6381fcb9e8d5b0d
GomathiKumar24 <gomathikumar240496@gmail.com>
2020-12-05 22:57:34 +0530

    download query model file

--- Vehicle-Finance/src/app/modules/query-model/query-model.component.ts
+++ Vehicle-Finance/src/app/modules/query-model/query-model.component.ts
@@ -933,10 +933,26 @@ export class QueryModelComponent implements OnInit, OnDestroy, AfterContentCheck
       this.getDownloadXlsFile(imageValue.imageUrl, imageValue.documentName, 'application/msword');
       return;
     }
-    if (imageValue.imageType.includes('png')) {
-      this.getDownloadXlsFile(imageValue.imageUrl, imageValue.documentName, 'application/png');
+    // const byteCharacters = atob(bas64String);
+    // const byteArrays = [];
 
-    }
+    // let  contentType: string='image/png', sliceSize=512;
+
+    // for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+    //   const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+    //   const byteNumbers = new Array(slice.length);
+    //   for (let i = 0; i < slice.length; i++) {
+    //     byteNumbers[i] = slice.charCodeAt(i);
+    //   }
+
+    //   const byteArray = new Uint8Array(byteNumbers);
+
+    //   byteArrays.push(byteArray);
+    // }
+
+    // const blob = new Blob(byteArrays, { type: contentType });
+    // return blob;
 
   }
 
