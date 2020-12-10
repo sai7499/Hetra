68989fe05a8e021f047ba840b25d0af937f30d69
sai7499 <saikrishna.y85@gmail.com>
2020-12-10 14:39:07 +0530

    modified valuation details

--- Vehicle-Finance/src/app/modules/dde/vehicle-valuation-router/valuation/valuation.component.ts
+++ Vehicle-Finance/src/app/modules/dde/vehicle-valuation-router/valuation/valuation.component.ts
@@ -27,7 +27,6 @@ import { environment } from 'src/environments/environment';
 export class ValuationComponent implements OnInit {
 
   vehicleValuationForm: FormGroup;
-
   leadId;
   colleteralId;
   public minDate = new Date(new Date().setFullYear(new Date().getFullYear() - 15));
@@ -150,7 +149,6 @@ export class ValuationComponent implements OnInit {
   permitType: any;
   permitDisabled: boolean;
   permitRequired: boolean;
-
   invalidPemitDate: boolean;
   invalidFitnessDate: boolean;
   invalidTaxDate: boolean;
@@ -166,8 +164,6 @@ export class ValuationComponent implements OnInit {
   accInPast: any;
   extValuator: boolean;
   disablePdfDownload: boolean;
-
-
   selectedDocDetails: DocRequest;
   showModal: boolean;
   isMobile: any;
@@ -176,7 +172,6 @@ export class ValuationComponent implements OnInit {
   longitude: string = null;
   documentArr: DocumentDetails[] = [];
   SELFIE_IMAGE: string;
-
   PROFILE_TYPE = Constant.PROFILE_ALLOWED_TYPES;
   OTHER_DOCUMENTS_SIZE = Constant.OTHER_DOCUMENTS_SIZE;
   OTHER_DOCS_TYPE = Constant.OTHER_DOCUMENTS_ALLOWED_TYPES;
@@ -883,6 +878,18 @@ export class ValuationComponent implements OnInit {
           this.modelInProdChange(this.vehicleValuationDetails.modelUnderProduction);
         }
       }
+      if ((this.vehicleValuationDetails.personInitiated !== null) && (!this.disableForm)) {
+        this.personInitiatedBy = this.vehicleValuationDetails.personInitiated;
+      } else if (this.vehicleValuationDetails.personInitiated === null) {
+        this.personInitiatedBy = this.userName;
+  
+      }
+      this.vehicleValuationForm.patchValue({
+        valuationInitiationDate: this.vehicleValuationDetails.valuationInitiationDate ?
+          this.utilityService.getDateFromString(this.vehicleValuationDetails.valuationInitiationDate) : '',
+        // personInitiated: this.vehicleValuationDetails.personInitiated || '',
+        personInitiated: this.personInitiatedBy ? this.personInitiatedBy : '',
+      })
       if (this.vehicleValuationDetails.valuatorRefNo) {
         this.setFormValue();
       }
@@ -1109,22 +1116,11 @@ export class ValuationComponent implements OnInit {
   setFormValue() {
 
     if (this.disableForm) {
-      // console.log('in disable state');
-
       this.yearMonthOfManufact = this.vehicleValuationDetails.yearOfManufacturer || '';
       this.personInitiatedBy = this.vehicleValuationDetails.personInitiated;
     } else {
       this.yearMonthOfManufacturer = this.vehicleValuationDetails.yearOfManufacturer ?
         this.utilityService.getDateFromString(this.vehicleValuationDetails.yearOfManufacturer) : '';
-      // console.log('in offline val', this.yearMonthOfManufacturer);
-    }
-    if ((this.vehicleValuationDetails.personInitiated !== null) && (!this.disableForm)) {
-      this.personInitiatedBy = this.vehicleValuationDetails.personInitiated;
-      // console.log('in not disable state and not null');
-    } else if (this.vehicleValuationDetails.personInitiated === null) {
-      this.personInitiatedBy = this.userName;
-      // console.log('in not disable state and  null');
-
     }
     this.vehicleValuationForm.patchValue({
       // valuatorType: this.vehicleValuationDetails.valuatorType || '',
@@ -1173,10 +1169,6 @@ export class ValuationComponent implements OnInit {
       seatingCapacity: this.vehicleValuationDetails.seatingCapacity || '',
       // speedometerReading: this.vehicleValuationDetails.speedometerReading || '',
       fuelUsed: this.vehicleValuationDetails.fuelUsed || '',
-      valuationInitiationDate: this.vehicleValuationDetails.valuationInitiationDate ?
-        this.utilityService.getDateFromString(this.vehicleValuationDetails.valuationInitiationDate) : '',
-      // personInitiated: this.vehicleValuationDetails.personInitiated || '',
-      personInitiated: this.personInitiatedBy ? this.personInitiatedBy : '',
       valuatorRefNo: this.vehicleValuationDetails.valuatorRefNo || '',
       borrowersName: this.vehicleValuationDetails.borrowersName || '',
       inspectionPlace: this.vehicleValuationDetails.inspectionPlace || '',
