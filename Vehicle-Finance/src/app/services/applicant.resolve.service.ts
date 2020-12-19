d7a74cbf209b60811bea1e0ee29ef1176d5f9aea
manimarran2 <manimarran.alagu@appiyo.com>
2020-12-18 18:17:12 -0800

    UDF implementation

--- Vehicle-Finance/src/app/services/applicant.resolve.service.ts
+++ Vehicle-Finance/src/app/services/applicant.resolve.service.ts
@@ -12,6 +12,7 @@ import { Applicant } from '@model/applicant.model';
 import { HttpService } from '@services/http.service';
 import { ApplicantService } from '@services/applicant.service';
 import { ApplicantDataStoreService } from '@services/applicant-data-store.service';
+import { formatDate, Location } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
@@ -21,21 +22,30 @@ export class ApplicantResolveService implements Resolve<boolean> {
   constructor(
     private httpService: HttpService,
     private applicantService: ApplicantService,
-    private applicantDataStoreService: ApplicantDataStoreService
+    private applicantDataStoreService: ApplicantDataStoreService,
+    private location: Location,
   ) {}
 
   resolve(
     router: ActivatedRouteSnapshot,
     RouterState: RouterStateSnapshot
   ): Observable<boolean> | boolean {
+    const url= this.location.path();
     const leadId = Number(router.params.leadId);
     const applicantId = Number(router.firstChild.params.applicantId);
     if (isNaN(leadId) || isNaN(applicantId)) {
       return;
     }
+    let groupId= 'APG008';
+    
     this.previousApplicantId = applicantId;
     const data = {
       applicantId,
+      "udfDetails": [
+        {
+          "udfGroupId": groupId,
+        }
+      ]
     };
     return this.applicantService.getApplicantDetail(data).pipe(
       map((value: any) => {
