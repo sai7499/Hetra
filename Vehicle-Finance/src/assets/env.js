7b8fc05dd678a336887d6b205457108ff8756301
scmkrishnaangular <r.86krishna@gmail.com>
2020-12-28 15:52:03 +0530

    janani bugs fixed

--- Vehicle-Finance/src/assets/env.js
+++ Vehicle-Finance/src/assets/env.js
@@ -8,8 +8,8 @@
     //window["env"]["userConfig"] = "@esfbuat.in";
     window["env"]["userConfig"] = "@equitasbank.in";
     window["env"]["useADAuth"] = false;
-    window["env"]["version"] = "0.32.6"; 
-    window["env"]["buildDate"] = "23-12-2020";
+    window["env"]["version"] = "0.32.8"; 
+    window["env"]["buildDate"] = "28-12-2020";
     window["env"]["sessionTime"] = "14";
     window["env"]["expriyAlertTime"] = "30";
   })(this);
