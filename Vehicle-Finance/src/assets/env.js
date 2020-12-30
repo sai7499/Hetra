a7a6e79f7615a2c1ea87aad605e9d657b59e2209
scmkrishnaangular <r.86krishna@gmail.com>
2020-12-30 22:55:30 +0530

    env changed

--- Vehicle-Finance/src/assets/env.js
+++ Vehicle-Finance/src/assets/env.js
@@ -1,14 +1,13 @@
 (function(window) {
-    window["env"] = window["env"] || {};
-  
-    // Environment variables
-    window["env"]["hostUrl"] = "/appiyo/";
-    //window["env"]["hostEnvironment"] = "UAT";
-    window["env"]["hostEnvironment"] = "DEV";
-    //window["env"]["userConfig"] = "@esfbuat.in";
-    window["env"]["userConfig"] = "@equitasbank.in";
-    window["env"]["useADAuth"] = false;
-    window["env"]["version"] = "0.31"; 
-    window["env"]["buildDate"] = "02-12-2020";
-  })(this);
-  
+  window["env"] = window["env"] || {};
+
+  // Environment variables
+  window["env"]["hostUrl"] = "/appiyo/";
+  //window["env"]["hostEnvironment"] = "UAT";
+  window["env"]["hostEnvironment"] = "DEV";
+  //window["env"]["userConfig"] = "@esfbuat.in";
+  window["env"]["userConfig"] = "@equitasbank.in";
+  window["env"]["useADAuth"] = false;
+  window["env"]["version"] = "0.31"; 
+  window["env"]["buildDate"] = "02-12-2020";
+})(this);
