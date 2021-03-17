1ecfe5527770b5fe46c012a14e6b5ee8d6661ba1
GomathiKumar24 <gomathikumar240496@gmail.com>
2021-03-17 12:12:53 +0530

    remove unwanted files

--- Vehicle-Finance/src/app/modules/shared/shared.module.ts
+++ Vehicle-Finance/src/app/modules/shared/shared.module.ts
@@ -50,7 +50,6 @@ import { SharedUserDefinedFieldsComponent } from './shared-user-defined-fields/s
 import { WindowModule } from '@progress/kendo-angular-dialog';
 import { CustomModalComponent } from './custom-modal/custom-modal.component';
 import { DeferralDocumentsComponent } from './deferral-documents/deferral-documents.component';
-import { AutocompleteLibModule } from 'angular-ng-autocomplete';
 setTheme('bs4');
 
 @NgModule({
@@ -99,7 +98,6 @@ setTheme('bs4');
     MatTreeModule,
     MatIconModule,
     WindowModule,
-    AutocompleteLibModule,
     ImageCropperModule,
   ],
   exports: [
