45daea80cdfbe5fab515d2f2ce3a29d5f8ad67ea
scmkrishnaangular <r.86krishna@gmail.com>
2021-03-22 21:11:35 +0530

    build released 47.5

--- Vehicle-Finance/src/assets/env.js
+++ Vehicle-Finance/src/assets/env.js
@@ -12,7 +12,7 @@
     window["env"]["useDEVADAuth"] = false; // for DEV
     window["env"]["useUATADAuth"] = true; // for UAT
     window["env"]["usePRDADAuth"] = true; // for production
-    window["env"]["version"] = "0.47.4"; 
+    window["env"]["version"] = "0.47.5"; 
     window["env"]["buildDate"] = "19-03-2021";
     window["env"]["sessionTime"] = "14";
     window["env"]["expriyAlertTime"] = "30";
